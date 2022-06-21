b2038706d51c9243298e052b0c156f956bf3fa52
cldborges <cldborges@yahoo.com.br>
2022-06-21 09:48:01 -0300

    teste heroku

--- classes.py
+++ classes.py
@@ -5,12 +5,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import os
 
 def extrair_jogos(url):
 
 #   driver = webdriver.Chrome(r"C:\Users\Claudio\Google Drive\Python\chromedriver.exe")
 #    driver = webdriver.Chrome()
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
+
+#    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
     jogos = driver.find_elements(By.CSS_SELECTOR, '.parent')
     while len(jogos) == 0:
